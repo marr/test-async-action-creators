141ed749631b0ebbc587ff5a8460257527b85ef6
Dave Marr <dave.marr@gmail.com>
2016-03-01 19:02:14 -0800

    Not able to test promise

--- actions.js
+++ actions.js
@@ -1,12 +1,22 @@
 import 'isomorphic-fetch'
 
+const checkResponse = res => {
+    if (res.ok) return res
+    const err = new Error(res.statusText)
+    err.response = res
+    throw err
+}
+
+const parseJSON = res => res.json()
+
 const loadedUser = user => {
     return { type: 'LOADED_USER', payload: user }
 }
 
 export const loadUser = id => (dispatch, getState) => {
-    console.log('fetching user', id)
-    return fetch(`/user/${id}.json`)
-        .then(res => dispatch(loadedUser(res.data)))
+    return fetch(`http://localhost/user/${id}.json`)
+        .then(checkResponse)
+        .then(parseJSON)
+        .then(user => dispatch(loadedUser(user)))
         .then(getState)
 }
--- actions.spec.js
+++ actions.spec.js
@@ -4,7 +4,7 @@ import { loadUser } from './actions'
 import sinon from 'sinon'
 import thunk from 'redux-thunk'
 
-nock('/')
+nock('http://localhost')
     .get('/user/1.json')
     .reply(200, {
         id: 1
@@ -17,10 +17,11 @@ describe('async actions', () => {
         const expectedActions = [
             { type: 'LOADED_USER', payload: { id: 1 }}
         ]
-        //const clock = sinon.useFakeTimers()
-        const store = mockStore({}, expectedActions, done)
-        store.dispatch(loadUser(1))
-        //clock.tick(1)
-        //clock.restore()
+        const store = mockStore({})
+       
+        // never gets inside the then here
+        store.dispatch(loadUser(1)).then(() => {
+            expect(store.getActions()).toEqual(expectedActions)
+        })
     })
 })
