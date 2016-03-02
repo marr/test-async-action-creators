81358ac325ed7eb17a1b745076eaabb906239ab8
Dave Marr <dave.marr@gmail.com>
2016-03-01 18:31:53 -0800

    redux-thunk applied

--- actions.spec.js
+++ actions.spec.js
@@ -1,6 +1,8 @@
 import nock from 'nock'
 import configureMockStore from 'redux-mock-store'
 import { loadUser } from './actions'
+import sinon from 'sinon'
+import thunk from 'redux-thunk'
 
 nock('/')
     .get('/user/1.json')
@@ -8,14 +10,17 @@ nock('/')
         id: 1
     })
 
-const mockStore = configureMockStore([])
+const mockStore = configureMockStore([thunk])
 
 describe('async actions', () => {
     it('updates state', done => {
         const expectedActions = [
             { type: 'LOADED_USER', payload: { id: 1 }}
         ]
+        //const clock = sinon.useFakeTimers()
         const store = mockStore({}, expectedActions, done)
         store.dispatch(loadUser(1))
+        //clock.tick(1)
+        //clock.restore()
     })
 })
